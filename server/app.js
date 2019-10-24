663af82194b87e3b29144cc17651bc1f4529532c
Aman Jaiswal <amanjaiswal@Aman-Jaiswal.local>
2019-10-24 23:25:12 +0200

    Deployed

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 var bodyParser = require("body-parser");
-var keys = require('./keys.js')
 
 const app = express();
 
@@ -23,7 +22,7 @@ var Users = require('./routes/Users')
 
 app.use('/users', Users)
 
-mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://test:test1234@libraryplatform-ebp2q.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
     console.log('connected to the database');
 })
